73a3d08495880a53ff8c32a6fecd4aee18f2e176
luiizsilverio <luiiz.silverio@gmail.com>
2023-05-18 20:58:23 -0300

    Aula 3 (31:14)

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,15 @@
-import { User } from "lucide-react";
-import Image from "next/image";
-
-import nlwLogo from "../assets/nlw-spacetime-logo.svg";
+import { CreateMemory } from "@/components/CreateMemory";
+import { Hero } from "@/components/Hero";
+import { SignIn } from "@/components/SignIn";
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className={`
-        flex flex-col items-start justify-between px-28 py-16 
+        flex flex-col items-start justify-between px-28 py-16
         relative overflow-hidden border-r border-gray-50/10
       `}>
+
         <div className={`
           absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50
           -translate-y-1/2 translate-x-1/2 rounded-full blur-full
@@ -17,48 +17,12 @@ export default function Home() {
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {/* Sign In */}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="text-sm leading-snug max-w-[140px]">
-            <span className="underline">Crie sua conta</span> e salve suas memórias!
-          </p>          
-        </a>       
-
-        {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a href="" 
-            className={`
-              inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase 
-              leading-none text-gray-900 hover:bg-green-600
-            `}
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
+        <SignIn />
+        <Hero />
       </div>
 
       <div className="flex flex-col p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[360px]">
-            Você ainda não registrou nenhuma lembrança, comece a {' '}
-            <a href="" className="underline hover:text-gray-50">criar agora!</a>
-          </p>
-        </div>
+        <CreateMemory />
       </div>
     </main>
   )
