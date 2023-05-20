4137a0f4b8f34f9d69c0b117b025da2fb751fbea
luiizsilverio <luiiz.silverio@gmail.com>
2023-05-20 11:36:04 -0300

    Aula 4 OK

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from "fastify";
+import { randomUUID } from "node:crypto";
+import { extname, resolve } from "node:path";
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline);
+
+export async function uploadRoutes(app: FastifyInstance) {
+
+  app.post('/upload', async (request, reply) => {
+    const data = await request.file({
+      limits: {
+        fileSize: 5242880, // limita arquivos de até 5MB
+      }
+    });
+
+    if (!data) {
+      return reply.status(400).send();
+    }
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(data.mimetype);
+
+    if (!isValidFileFormat) {
+      return reply.status(400).send();
+    }
+
+    const fileId = randomUUID();
+    const extension = extname(data.filename);
+    const fileName = fileId.concat(extension);
+
+    const stream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName)
+    )
+
+    await pump(data.file, stream);
+
+    const fullUrl = request.protocol.concat('://').concat(request.hostname);
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString();
+
+    return { ok: true };
+  })
+
+}
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,13 @@ import 'dotenv/config';
 import fastify from "fastify";
 import jwt from '@fastify/jwt';
 import cors from '@fastify/cors';
+import multipart from '@fastify/multipart';
+import $static from '@fastify/static';
+import { resolve } from 'node:path';
+
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
 
 const app = fastify();
 
@@ -11,11 +16,19 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(multipart);
+
+app.register($static, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(jwt, {
   secret: 'spacetime'
 })
 
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 const port = 3333;
